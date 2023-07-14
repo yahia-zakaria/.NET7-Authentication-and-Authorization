bc0490cbe0a0c87762ff1b6c6f1bc1b95bf37be2
yahia-zakaria <yahiazakaria91@hotmail.com>
2023-07-14 18:00:12 +0300

    add jwt token server

---
+++ Client/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Server.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly IConfiguration _configuration;
+
+		public HomeController(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[Authorize]
+		public IActionResult Secret()
+		{
+			return View();
+		}
+
+		[AllowAnonymous]
+		public IActionResult Authenticate(string returnUrl)
+		{
+			var claims = new[]
+			{
+				new Claim(JwtRegisteredClaimNames.Sub, Guid.NewGuid().ToString()),
+				new Claim(ClaimTypes.Name, "Yahia Yagoub"),
+				new Claim(ClaimTypes.Email, "Yahia@hotmail.com"),
+				new Claim(ClaimTypes.DateOfBirth, "11/11/2000"),
+			};
+
+			var keyBytes = Encoding.UTF8.GetBytes(_configuration.GetSection("JwtToken")["Secret"]);
+			var key = new SymmetricSecurityKey(keyBytes);
+
+			var jwtSecurity = new JwtSecurityToken(
+				_configuration.GetSection("JwtToken")["Issuer"],
+				_configuration.GetSection("JwtToken")["Audience"],
+				claims,
+				notBefore: DateTime.Now,
+				expires: DateTime.Now.AddHours(1),
+				new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+				);
+
+			var token = new JwtSecurityTokenHandler().WriteToken(jwtSecurity);
+			return Ok(token);
+		}
+	}
+}
---
+++ Server/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication("OAuth")
+	.AddJwtBearer("OAuth", config =>
+	{
+		config.Events = new JwtBearerEvents()
+		{
+			OnMessageReceived = context =>
+			{
+				if (context.Request.Query.ContainsKey("access_token"))
+				{
+					context.Token = context.Request.Query["access_token"];
+				}
+				return Task.CompletedTask;
+			}
+		};
+		var keyBytes = Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JwtToken")["Secret"]);
+		var key = new SymmetricSecurityKey(keyBytes);
+		config.TokenValidationParameters = new TokenValidationParameters
+		{
+			//ClockSkew = TimeSpan.Zero,
+			ValidAudience = builder.Configuration.GetSection("JwtToken")["Audience"],
+			ValidIssuer = builder.Configuration.GetSection("JwtToken")["Issuer"],
+			IssuerSigningKey = key
+		};
+	});
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapDefaultControllerRoute();
+app.Run();
